26f00e3426e5e799d44f8f0ff5413d20dcab9cfd
Lucky Sah <lk4179869@gmail.com>
2026-08-02 11:51:59 +0530

    add: Gamerscore activity

---
+++ gameScore/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import GameScore from './GameScore'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <GameScore/>
+          </>
+  )
+}
+
+export default App
