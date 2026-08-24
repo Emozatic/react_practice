0a190f60f2f9ee268610ca74c58358729aabb207
Lucky Sah <lk4179869@gmail.com>
2026-08-24 20:02:53 +0530

    add: component

--- 1/src/App.jsx
+++ 1/src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import './App.css'
+import Title from './Title';
+import Description from './Description';
 
 function App() {
   
@@ -7,6 +9,8 @@ function App() {
   return (
     <>
     <h1>Hello World!</h1>
+    <Title/>
+    <Description/>
     </>
   )
 }
---
+++ 1/src/Description.jsx
@@ -0,0 +1,6 @@
+function Description (){
+    return(
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam obcaecati cupiditate error dolores debitis iusto accusamus qui eveniet tempore! Autem illum repellendus omnis dolor explicabo. Sed nobis facilis quod architecto?</p>
+    )
+}
+export default Description;
