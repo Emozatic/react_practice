0a8fb181b983f84ea1106c6f35218b286ba3de2a
Lucky Sah <lk4179869@gmail.com>
2026-08-25 20:00:30 +0530

    add: onchange event

--- 4events/src/App.jsx
+++ 4events/src/App.jsx
@@ -2,13 +2,17 @@
 import './App.css'
 
 function App() {
- let handleClick=()=>{
-  console.log("Button Clicked");
+ let handleClick=(event)=>{
+  console.log(event);
+ }
+ let change= (event)=>{
+  console.log(event.target.value);
  }
 
   return (
     <>
   <button onClick={handleClick}>Click me</button>
+  <input type="text" placeholder='enter anything' onChange={change}/>
      </>
   )
 }
