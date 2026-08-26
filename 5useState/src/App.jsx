7eeedee9f539efe279cd24148c92c8be9870d88a
Lucky Sah <lk4179869@gmail.com>
2026-08-26 08:32:51 +0530

    add: inc and dec activity

--- 5useState/src/App.jsx
+++ 5useState/src/App.jsx
@@ -6,10 +6,19 @@ const[count,setCount]=useState(0);
 function increament(){
   setCount(count+1);
 }
+function dec(){
+  if(count>0){
+  setCount(count-1);
+  }
+  else{
+    setCount("negative")
+  }
+}
   return (
     <>
       <h1>{count}</h1>
       <button onClick={increament}>Inc</button>
+      <button onClick={dec}>Decreament</button>
 
           </>
   )
