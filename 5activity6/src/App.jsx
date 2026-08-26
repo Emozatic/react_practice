7f3f36a9ad000252c9e7a993143dfd4f923da544
Lucky Sah <lk4179869@gmail.com>
2026-08-26 19:15:59 +0530

    add: activity

--- 5activity6/src/App.jsx
+++ 5activity6/src/App.jsx
@@ -5,17 +5,21 @@ function App() {
   const [user, setUser]= useState({
     name:"Lucky",
     age:21,
-    city:"chandigarh"
+    address:{
+      city:"Chandigarh",
+      pincode: 160001
+    }
   })
   function changename(){
-    setUser((prevUser)=>({...prevUser, name:"Rahul", age:22}))
+    setUser(prevUser=>({...prevUser, address:{...prevUser.address, city:"Kalka", pincode:173220}}))
   }
 
   return (
     <>
     <h1>{user.name}</h1>
     <h1>{user.age}</h1>
-    <h1>{user.city}</h1>
+    <h1>{user.address.city}</h1>
+    <h1>{user.address.pincode}</h1>
     <button onClick={changename}>change name</button>
       </>
   )
