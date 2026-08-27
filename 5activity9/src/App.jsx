da39ba921df070d857c4657ba11ebb08567e967c
Lucky Sah <lk4179869@gmail.com>
2026-08-27 21:25:59 +0530

    add: activity map method

--- 5activity9/src/App.jsx
+++ 5activity9/src/App.jsx
@@ -6,15 +6,24 @@ function App() {
   function add(){
     setCount((items)=>([...items,"Node"]))
   }
+  function filteratin(){
+    setCount((items)=>(
+  items.filter((ele,idx,array)=>(
+        ele!== "Node"
+  ))
+    ))
+  }
 
   return (
     <>
   <h1>{count[0]}</h1>
   <h2>{count[1]}</h2>
   <button onClick={add}>Add</button>
+  <button onClick={filteratin}>Del</button>
   <ul>{count.map((item)=>(
     <li>{item}</li>
   ))}</ul>
+  
      </>
   )
 }
