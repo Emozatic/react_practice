ff9f7ea2fcd9de48e15a0266127eb718af78a485
Lucky Sah <lk4179869@gmail.com>
2026-08-28 19:55:01 +0530

    add: activity

--- 5activity10/src/App.jsx
+++ 5activity10/src/App.jsx
@@ -2,12 +2,22 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState([])
+  const [count, setCount] = useState(["JavaScript", "React"])
+
+  function adding(){
+    setCount((previous)=>([
+      ...previous, "Node"
+    ]))
+  }
 
 
   return (
     <>
-      
+      <ul>{count.map((data, idx)=>(
+        <li key={idx}>{data}
+        </li>
+      ))}</ul>
+      <button onClick={adding}>Add</button>
           </>
   )
 }
