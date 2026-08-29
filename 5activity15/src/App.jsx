92ae85812767030446c3151207c350de2bd49075
Lucky Sah <lk4179869@gmail.com>
2026-08-29 09:38:51 +0530

    add: remove specific items

--- 5activity15/src/App.jsx
+++ 5activity15/src/App.jsx
@@ -2,12 +2,19 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [skills, setSkills]= useState(["React", "Node"]);
+  const [skills, setSkills]= useState(["React", "Node", "React"]);
 
   function add(){
     setSkills((prevSkills)=>([
-     prevSkills[0], "Js", ...prevSkills.slice(1)
+     prevSkills[0], "Js", ...prevSkills.splice(1,1)
     ]))
+  }
+  function delNode(){
+    setSkills((prevSkills)=>(
+      prevSkills.filter((items)=>(
+        items !== "Node"
+      ))
+    ))
   }
 
   return (
@@ -16,6 +23,7 @@ function App() {
       <h1>{skill}</h1>
     ))}
     <button onClick={add}>Add</button>
+    <button onClick={delNode}>Del</button>
       </>
   )
 }
