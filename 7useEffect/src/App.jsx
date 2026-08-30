7dac84a764170bd246f6344cbec4b9fb49cf3013
Lucky Sah <lk4179869@gmail.com>
2026-08-30 10:12:04 +0530

    add: first program of useEffect

---
+++ 7useEffect/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import {useEffect} from 'react'
+import './App.css'
+
+function App() {
+  useEffect(()=>{
+    console.log("Hello, World!");
+  })
+  function app(){
+    con
+  }
+
+  return (
+    <>
+    <h2>{app}</h2>
+    <h1>Hello</h1>
+      </>
+  )
+}
+
+export default App
