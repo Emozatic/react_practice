90e4cce3a4fe6ffdb172dacc537d4ae04dd2dc68
Lucky Sah <lk4179869@gmail.com>
2026-08-30 09:14:18 +0530

    add: activity form validation

--- 6activity4/src/App.jsx
+++ 6activity4/src/App.jsx
@@ -20,8 +20,26 @@ function App() {
     })
   }
 
+  function submission(e){
+    e.preventDefault();
+    if(form.name==""){
+      console.log("enter a valid name");
+    }
+    if(form.age==""){
+      console.log("enter a valid age")
+      if(form.age<18){
+        console.log("enter age above 18")
+      }
+    };
+
+  }
+
   return (
     <>
+    <form onSubmit={submission}>
+      <input type="text" value={form.name} onChange={setting} placeholder='enter your name' name='name'/>
+      <input type="text" value={form.age} onChange={setting} placeholder='enter your age' name='age'/>
+      <input type="text" value={form.email} onChange={setting} placeholder='enter your email' name='email'/>
     <input type="textarea" value={form.message} onChange={setting} placeholder='enter your message' name='message'/>
     <select name="country" value={form.country} onChange={setting}>
       <option value="">Select Country</option>
@@ -31,7 +49,8 @@ function App() {
     </select>
     <h3>{form.country}</h3>
     <h4>{form.message}</h4>
-    
+    <button type='submit'>Submit</button>
+    </form>
           </>
   )
 }
