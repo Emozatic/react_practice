57c6a07eee2393a767107926da91a1273534a2ca
Lucky Sah <lk4179869@gmail.com>
2026-08-30 12:12:03 +0530

    add: error interface

--- 7useEffect/src/App.jsx
+++ 7useEffect/src/App.jsx
@@ -6,16 +6,24 @@ function App() {
   const[count, setCount]= useState(0);
   const[user, setUser]= useState([])
   const[loading, setLoading]= useState(true);
+  const[error, setError]= useState("");
 //   useEffect(()=>{
 //     console.log("Hello, World!");
 // },[count]);
 
 useEffect(()=>{
   fetch("https://jsonplaceholder.typicode.com/users")
-  .then((res)=>res.json())
+  .then((res)=>{
+    if(!res.ok){
+      throw new Error ("Something went wrong")
+    }
+    return res.json()})
   .then((data)=>{
     console.log(data);
     setUser(data);
+    setLoading(false)
+  }).catch((err)=>{
+    setError(err.message);
     setLoading(false)
   })
 },[])
@@ -25,7 +33,7 @@ useEffect(()=>{
 
   return (
     <>
-    {loading ? (<h1>Loading...</h1>) : 
+    {loading ? (<h1>Loading...</h1>) : error ? <h2>{error}</h2> :
     <ul>{user.map((data)=>(
       <div key={data.id}>
       <li>{data.name}</li>
