9ca5df29006cd0d2f6919fb916cc8ef87e5b9443
Lucky Sah <lk4179869@gmail.com>
2026-09-02 08:17:55 +0530

    add: activities for useRef

--- 9useRef/src/App.jsx
+++ 9useRef/src/App.jsx
@@ -2,8 +2,10 @@ import { useState } from 'react'
 import './App.css'
 import {useRef} from 'react'
 function App() {
+  const [count, setCount]= useState(0);
   const inputRef= useRef();
   const buttonRef= useRef();
+  const countRef= useRef(0);
 
   function focusInp(){
     inputRef.current.focus();
@@ -12,14 +14,20 @@ function App() {
     buttonRef.current.focus();
     console.log("button clicked")
   }
+  function changeValue(){
+    setCount(count+1);
+  }
 
 
   return (
     <>
-    <input ref={inputRef}/>
+  <h1>{countRef.current}</h1>
+  <h2>{count}</h2>
+    {/* <input ref={inputRef}/>
     <button ref={buttonRef}>button for focus</button>
     <button onClick={focusInp}>Focus</button>
-    <button onClick={focusButton}>Focus Btn</button>
+    <button onClick={focusButton}>Focus Btn</button> */}
+    <button onClick={changeValue}>Update count</button>
       </>
   )
 }
