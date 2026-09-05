4daea54ac4b5f1452aedf17b9976f550c5b50d29
Lucky Sah <lk4179869@gmail.com>
2026-09-05 16:53:54 +0530

    add: nested routes

---
+++ 17reactRouterDom2/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import {BrowserRouter, Routes, Route, NavLink} from "react-router-dom"
+import './App.css'
+import Dashboard from './Dashboard'
+import Setting from './Setting'
+import Profile from './Profile'
+
+function App() {
+
+
+  return (
+    <>
+    <BrowserRouter>
+    <NavLink to="/dashboard">Dashboard</NavLink>
+    <NavLink to="/dashboard/profile/setting">Setting</NavLink>
+    <Routes> 
+      <Route path='/dashboard' element={<Dashboard/>}>
+      <Route path='profile' element={<Profile/>}>
+      <Route path='setting' element={<Setting/>}/>
+      </Route>
+      </Route>
+
+    </Routes> 
+    </BrowserRouter>
+    </>
+  )
+}
+
+export default App
---
+++ 17reactRouterDom2/src/Profile.jsx
@@ -0,0 +1,10 @@
+import { Outlet } from "react-router-dom"
+function Profile(){
+    return(
+        <>
+        <h1>Profile</h1>
+        <Outlet/>
+        </>
+    )
+}
+export default Profile;
