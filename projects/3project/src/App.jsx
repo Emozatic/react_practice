da032dc4294e27d10423925ad4e950e15fe04f01
Lucky Sah <lk4179869@gmail.com>
2026-09-14 20:16:41 +0530

    add: props and product comp in 3project

--- projects/3project/src/App.jsx
+++ projects/3project/src/App.jsx
@@ -1,10 +1,16 @@
 import './App.css'
+import Product from './components/Product'
 
 function App(){
 
   return (
     <>
      <h1>My E-commerce Website</h1>
+    <Product name="Iphone" price={100000} category="electronics"/>
+    <hr />
+    <Product name="Laptop" price={29999} category="electronics"/>
+    <hr />
+    <Product name="Shirt" price={299} category="fashion"/>
     </>
   )
 }
---
+++ projects/3project/src/components/Product.jsx
@@ -0,0 +1,11 @@
+function Product({name, price, category}){
+    return(
+        <>
+        <h1>Name:- {name}</h1>
+        <h2>Price:- {price}</h2>
+        <h2>Category:- {category}</h2>
+        </>
+    )
+}
+
+export default Product;
