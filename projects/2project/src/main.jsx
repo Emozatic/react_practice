2348779a34e438e261da7e30b48ec2f89d9b097a
Lucky Sah <lk4179869@gmail.com>
2026-09-14 20:06:57 +0530

    add: creating new 3project

--- projects/2project/src/main.jsx
+++ projects/2project/src/main.jsx
@@ -3,11 +3,14 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter} from 'react-router-dom'
 import CartProvider from './components/CartProvider.jsx'
+import AuthProvider from './components/AuthProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
     <BrowserRouter>
     <CartProvider>
+        <AuthProvider>
     <App />
+    </AuthProvider>
     </CartProvider>
     </BrowserRouter>
 )
--- projects/2project/src/pages/Login.jsx
+++ projects/2project/src/pages/Login.jsx
@@ -1,6 +1,21 @@
+import {  useState } from "react";
 function Login(){
+    const [data, setData]= useState({email:"", password:""})
+    function change(e){
+        setData((prev)=>({
+            ...prev, [e.target.name]: e.target.value
+    }))
+    }
+    function handleSubmit(e){
+        e.preventDefault();
+        console.log(data)
+    }
     return(
-        <h1>Login</h1>
+        <form onSubmit={handleSubmit}>
+            <input type="text" value={data.email} onChange={change} name="email"/>
+            <input type="text" value={data.password} onChange={change} name="password"/>
+            <button type="submit">Login</button>
+        </form>
     )
 }
 export default Login;
