52e7a1c9fc39aa49c18d2755278874986dd77be3
Lucky Sah <lk4179869@gmail.com>
2026-09-15 19:10:32 +0530

    add: presenting in product array

--- projects/3project/src/App.jsx
+++ projects/3project/src/App.jsx
@@ -2,15 +2,16 @@ import './App.css'
 import Product from './components/Product'
 
 function App(){
+  let products= [
+    {id:1, name:"Iphone 15" , price: 50000, category:"mobile"},
+    {id:2, name:"HP laptop" , price: 100000, category:"Laptop"},
+    {id:1, name:"Logitech Mouse" , price: 500, category:"Accessories"},
+  ]
 
   return (
     <>
      <h1>My E-commerce Website</h1>
-    <Product name="Iphone" price={100000} category="electronics"/>
-    <hr />
-    <Product name="Laptop" price={29999} category="electronics"/>
-    <hr />
-    <Product name="Shirt" price={299} category="fashion"/>
+    <Product products= {products}/>
     </>
   )
 }
--- projects/3project/src/components/Product.jsx
+++ projects/3project/src/components/Product.jsx
@@ -1,9 +1,19 @@
-function Product({name, price, category}){
+function Product({products}){
+    console.log(products)
+    let check= products.map((items)=>{
+        console.log(items.name);
+    })
     return(
         <>
-        <h1>Name:- {name}</h1>
-        <h2>Price:- {price}</h2>
-        <h2>Category:- {category}</h2>
+        {products.map((item)=>(
+            <div key={item.id}>
+                <h1>ID:- {item.id}</h1>
+                <h2> Name:- {item.name}</h2>
+                <p>Price:- {item.price}</p>
+                <p>Category:- {item.category}</p>
+                <hr />
+            </div>
+        ))}
         </>
     )
 }
