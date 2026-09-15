0679fcf569c3a8962db976b1583f2a2937eeced3
Lucky Sah <lk4179869@gmail.com>
2026-09-15 20:08:42 +0530

    add: adding props from parent to child

--- projects/3project/src/App.jsx
+++ projects/3project/src/App.jsx
@@ -5,13 +5,25 @@ function App(){
   let products= [
     {id:1, name:"Iphone 15" , price: 50000, category:"mobile"},
     {id:2, name:"HP laptop" , price: 100000, category:"Laptop"},
-    {id:1, name:"Logitech Mouse" , price: 500, category:"Accessories"},
+    {id:3, name:"Logitech Mouse" , price: 500, category:"Accessories"},
   ]
+
+  function handleProductClick(product){
+    console.log(product);
+  }
 
   return (
     <>
      <h1>My E-commerce Website</h1>
-    <Product products= {products}/>
+
+    {products.map((item)=>(
+      <>
+      <div key={item.id}>
+      <Product name= {item.name} price={item.price} category={item.category} id={item.id} handleProductClick={handleProductClick}/>
+      </div>
+      <hr />
+      </>
+    ))}
     </>
   )
 }
--- projects/3project/src/components/Product.jsx
+++ projects/3project/src/components/Product.jsx
@@ -1,19 +1,24 @@
-function Product({products}){
-    console.log(products)
-    let check= products.map((items)=>{
-        console.log(items.name);
-    })
+function Product({id, name, price, category, handleProductClick}){
+    // function viewDetails(name){
+    //     console.log(`${name} clicked`);
+    // }
     return(
         <>
-        {products.map((item)=>(
+        {/* {products.map((item)=>(
             <div key={item.id}>
                 <h1>ID:- {item.id}</h1>
                 <h2> Name:- {item.name}</h2>
                 <p>Price:- {item.price}</p>
                 <p>Category:- {item.category}</p>
+                <button onClick={()=>{viewDetails(item)}}>View Details</button>
                 <hr />
             </div>
-        ))}
+        ))} */}
+            <h1>ID:- {id}</h1>
+            <h1>Name:- {name}</h1>
+            <h2>Price:- {price}</h2>
+            <h2>Category:- {category}</h2>
+            <button onClick={()=>{handleProductClick(name)}}>View {name}</button>
         </>
     )
 }
