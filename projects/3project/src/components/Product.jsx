82466d8258d752664725fd1b6b3d9b4748a051d5
Lucky Sah <lk4179869@gmail.com>
2026-09-15 20:14:57 +0530

    showing details in console through button

--- projects/3project/src/components/Product.jsx
+++ projects/3project/src/components/Product.jsx
@@ -1,24 +1,12 @@
 function Product({id, name, price, category, handleProductClick}){
-    // function viewDetails(name){
-    //     console.log(`${name} clicked`);
-    // }
+    
     return(
         <>
-        {/* {products.map((item)=>(
-            <div key={item.id}>
-                <h1>ID:- {item.id}</h1>
-                <h2> Name:- {item.name}</h2>
-                <p>Price:- {item.price}</p>
-                <p>Category:- {item.category}</p>
-                <button onClick={()=>{viewDetails(item)}}>View Details</button>
-                <hr />
-            </div>
-        ))} */}
             <h1>ID:- {id}</h1>
             <h1>Name:- {name}</h1>
             <h2>Price:- {price}</h2>
             <h2>Category:- {category}</h2>
-            <button onClick={()=>{handleProductClick(name)}}>View {name}</button>
+            <button onClick={()=>{handleProductClick({id, name, price, category})}}>View {name}</button>
         </>
     )
 }
