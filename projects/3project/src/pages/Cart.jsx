d525dce6d83f85143d8f730579e3073ef7b85b38
Lucky Sah <lk4179869@gmail.com>
2026-09-17 22:35:37 +0530

    add: cart clear option

--- projects/3project/src/pages/Cart.jsx
+++ projects/3project/src/pages/Cart.jsx
@@ -3,18 +3,28 @@ import CartContext from "../context/CartContext";
 
 function Cart(){
     const {cart, setCart}= useContext(CartContext);
+
     function removeFromCart(product){
         
         setCart(cart.filter((item)=>item.id !== product.id ))
     }
+
     function incQuantity(product){
         setCart(cart.map((item)=> item.id === product.id ? 
         {...item, quantity:item.quantity+1} : item))
         }
 
-        function decQuantity(product){
+    function decQuantity(product){
         setCart(cart.map((item)=> item.id === product.id ? 
         {...item, quantity:item.quantity>1 ? item.quantity-1 : 1} : item))
+        }
+
+        let total= cart.reduce((total, item)=>{
+            return total+item.price*item.quantity
+        },0)
+
+        function clearCart(){
+            setCart([])
         }
 
     
@@ -28,8 +38,9 @@ function Cart(){
             <h3>{item.price}</h3>
             <h3>Category:- {item.category}</h3>
             <h3>Quantity:- <button onClick={()=>{incQuantity(item)}}>+</button>{item.quantity}<button onClick={()=>{decQuantity(item)}}>-</button></h3>
-            
             <button onClick={()=>{removeFromCart(item)}}>Remove</button>
+            <button onClick={clearCart}>Clear</button>
+            <h4>Total:- {total}</h4>
             </div>
         ))}
         </>
