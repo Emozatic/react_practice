077d60dba4a9014339b4a943ccd9846c26249295
Lucky Sah <lk4179869@gmail.com>
2026-09-17 21:44:14 +0530

    add: quantity add and remove

--- projects/3project/src/pages/Cart.jsx
+++ projects/3project/src/pages/Cart.jsx
@@ -7,6 +7,17 @@ function Cart(){
         
         setCart(cart.filter((item)=>item.id !== product.id ))
     }
+    function incQuantity(product){
+        setCart(cart.map((item)=> item.id === product.id ? 
+        {...item, quantity:item.quantity+1} : item))
+        }
+
+        function decQuantity(product){
+        setCart(cart.map((item)=> item.id === product.id ? 
+        {...item, quantity:item.quantity>1 ? item.quantity-1 : 1} : item))
+        }
+
+    
     return(
         <>
         <h1>Cart</h1>
@@ -15,8 +26,9 @@ function Cart(){
             <img src={item.image} alt={item.name} />
             <h2>{item.name}</h2>
             <h3>{item.price}</h3>
-            <h3>{item.category}</h3>
-            <h3>{item.quantity}</h3>
+            <h3>Category:- {item.category}</h3>
+            <h3>Quantity:- <button onClick={()=>{incQuantity(item)}}>+</button>{item.quantity}<button onClick={()=>{decQuantity(item)}}>-</button></h3>
+            
             <button onClick={()=>{removeFromCart(item)}}>Remove</button>
             </div>
         ))}
