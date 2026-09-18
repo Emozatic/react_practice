00ae5df56d7cf2dd7a62893ae0c0313ca2637e63
Lucky Sah <lk4179869@gmail.com>
2026-09-18 22:33:52 +0530

    add: localstorage

--- projects/3project/src/context/CartContext.jsx
+++ projects/3project/src/context/CartContext.jsx
@@ -1,3 +1,6 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 const CartContext= createContext();
+const [cart,setCart]= useState(()=>{const savedCart= localStorage.getItem("cart");
+    return savedCart ? JSON.parse(savedCart) : [];
+})
 export default CartContext;
