1ccc980fc449a424436bcd6cecf65d414c19544a
Lucky Sah <lk4179869@gmail.com>
2026-09-19 11:36:06 +0530

    add: localstorage for cart

--- projects/3project/src/context/CartContext.jsx
+++ projects/3project/src/context/CartContext.jsx
@@ -1,6 +1,3 @@
 import { createContext, useState, useEffect } from "react";
 const CartContext= createContext();
-const [cart,setCart]= useState(()=>{const savedCart= localStorage.getItem("cart");
-    return savedCart ? JSON.parse(savedCart) : [];
-})
 export default CartContext;
--- projects/3project/src/context/CartProvider.jsx
+++ projects/3project/src/context/CartProvider.jsx
@@ -1,7 +1,12 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import CartContext from "./CartContext"
 function CartProvider({children}){
-const [cart,setCart]= useState([]);
+const [cart,setCart]= useState(()=>{const savedCart= localStorage.getItem("cart");
+    return savedCart ? JSON.parse(savedCart) : [];
+})
+        useEffect(()=>{
+    localStorage.setItem("cart", JSON.stringify(cart))
+},[cart])
     return(
         <>
         <CartContext.Provider value={{cart, setCart}}>{children}</CartContext.Provider>
